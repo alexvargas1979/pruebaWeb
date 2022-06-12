183e5437c597b698b92130e3028dd05c33bff857
Carlos <desarrollo@innova-id.com>
2022-06-12 08:58:02 -0400

    primer commit

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express')
+
+const app = express()
+
+app.get('/', (req, res) => {
+
+    console.log('peticion recibida')
+    res.send('<h1>hola mundo </h1>')
+})
+
+app.listen(4000,()=> {
+    console.log('servidor escuchando')
+})
